f336a4a98da7257b15c92626701f9ead31837764
ShahilMangroliya <vrutikmangroliya123@gmail.com>
2023-09-25 16:28:21 +0530

    program cpp1

---
+++ hello.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    cout << "1 hello word!" << endl;
+    cout << "2 hello word!";
+    return 0;
+}
---
+++ introducation.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    cout << "name          : vrutik mangroliya" << endl;
+    cout << "age           :21 year" << endl;
+    cout << "birth place   :amreli" << endl;
+    cout << "birthday date :24/12" << endl;
+    cout << "hobies        :trawling ,designe" << endl;
+    return 0;
+}
