284bc201f4b9654fdf41e4ad91bd69124abdfb8f
ShahilMangroliya <vrutikmangroliya123@gmail.com>
2023-11-06 11:16:32 +0530

    con

--- constructor/Bank_acc.cpp
+++ constructor/Bank_acc.cpp
@@ -2,42 +2,38 @@
 using namespace std;
 class bankaccount
 {
-     private :
- int amt, dep, wit, total, withd, balance;
+private:
+     int amt, dep, wit, total, withd, balance;
 
-     public :
-      int a = amt, d = dep, w = wit, t = total, wd = withd, b = balance;
+public:
+     int a = amt, d = dep, w = wit, t = total, wd = withd, b = balance;
      bankaccount()
-    {
-         cout << "Enter your amount=  ";
-         cin >> a;
-         cout << "Enter your deposit amount= ";
-         cin >> d;
-    }
+     {
+          cout << "Enter your amount=  ";
+          cin >> a;
+          cout << "Enter your deposit amount= ";
+          cin >> d;
+     }
      void deposit()
-    {
-         t = d + a;
-         cout << "Total amount in your account= " << t << endl;
-        
-    }
-    void withdraw()
-    {
-         cout << "Enter your withdraw_amount= ";
-         cin >> wd;
-         if (a < wd)
-        {
-             cout << "you have not sufficient amount";
-            
-        }
-         else if (a >= w)
+     {
+          t = d + a;
+          cout << "Total amount in your account= " << t << endl;
+     }
+     void withdraw()
+     {
+          cout << "Enter your withdraw_amount= ";
+          cin >> wd;
+          if (a < wd)
+          {
+               cout << "you have not sufficient amount";
+          }
+          else if (a >= w)
 
-        {
-             b = t - wd;
-             cout << "Present amount in your account= " << b << endl;
-        }
-
-        
-    }
+          {
+               b = t - wd;
+               cout << "Present amount in your account= " << b << endl;
+          }
+     }
 };
 int main()
 {
